fb7496fc67023dbbe0e281216b0552d05211f941
Parthasarathy Lab, Dept. of Physics, University of Oregon <rplab@users.noreply.github.com>
2022-09-19 19:31:56 -0700

    Update circling.py

--- circling/circling.py
+++ circling/circling.py
@@ -7,7 +7,6 @@
 # ---------------------------------------------------------------------------
 from math import sqrt
 from toolkit import *
-import numpy as np
 from circle_fit_taubin import TaubinSVD
 # ---------------------------------------------------------------------------
 
@@ -25,13 +24,13 @@ def get_rmse(distances_lst):
     return sqrt(np.sum(distances_lst))
 
 
-def get_circling_wf(fish1_data, fish2_data, angle_data, window_size):
+def get_circling_wf(fish1_pos, fish2_pos, angle_data, window_size):
     idx_1, idx_2, wf = 0, window_size, window_size
     circling_wf = np.array([])
     
     while idx_2 <= 15000:   # end of the array for both fish
-        head_temp = np.concatenate((fish1_data[idx_1:idx_2], 
-        fish2_data[idx_1:idx_2]), axis=0)
+        head_temp = np.concatenate((fish1_pos[idx_1:idx_2], 
+        fish2_pos[idx_1:idx_2]), axis=0)
         taubin_output = TaubinSVD(head_temp)  # output gives (x_c, y_c, r)
         reciprocal_radius = get_reciprocal_radius(taubin_output)
         distances_temp = np.empty(2 * window_size)
@@ -42,20 +41,9 @@ def get_circling_wf(fish1_data, fish2_data, angle_data, window_size):
         rmse = get_rmse(distances_temp)
 
         if (reciprocal_radius >= 0.005 and rmse < 25 and 
-        -1 <= get_opposite_orientation_angle(angle_data, idx_1, idx_2) < -0.9): 
+        (check_antiparallel_criterion(angle_data, idx_1, idx_2, -1, -0.9, 
+        fish1_pos, fish2_pos))): 
             circling_wf = np.append(circling_wf, wf)
     
         idx_1, idx_2, wf = idx_1+window_size, idx_2+window_size, wf+window_size
     return circling_wf
-            
-
-def main():
-    pos_data = load_data("results_SocPref_3c_2wpf_k1_ALL.csv", 3, 5)
-    angle_data = load_data("results_SocPref_3c_2wpf_k1_ALL.csv", 5, 6)
-    circling_wfs = get_circling_wf(pos_data[0], pos_data[1], angle_data, 10)
-
-
-
-
-if __name__ == '__main__':
-    main()
