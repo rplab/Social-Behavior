a6d35fb81ba9e725bf2dbeb221c8ff1583cf3830
Parthasarathy Lab, Dept. of Physics, University of Oregon <rplab@users.noreply.github.com>
2022-11-03 11:12:19 -0700

    Update toolkit.py

--- toolkit.py
+++ toolkit.py
@@ -15,9 +15,9 @@ def load_data(dir, idx_1, idx_2):
     return fish1_data, fish2_data
 
 
-def get_antiparallel_angle(angle_data, idx_1, idx_2):
-    fish1_data = angle_data[0][idx_1:idx_2]
-    fish2_data = angle_data[1][idx_1:idx_2]
+def get_antiparallel_angle(fish1_angle_data, fish2_angle_data, idx_1, idx_2):
+    fish1_data = fish1_angle_data[idx_1:idx_2]
+    fish2_data = fish2_angle_data[idx_1:idx_2]
     theta_avg = np.mean(np.cos(np.subtract(fish1_data, fish2_data)))
     return theta_avg
 
@@ -28,14 +28,25 @@ def get_head_distance(fish1_pos, fish2_pos, idx_1, idx_2):
     dist_temp = head_dist_matrix[idx_1:idx_2]
     avg_dist = np.average(dist_temp, axis=0)
     return avg_dist
-    
 
-def check_antiparallel_criterion(angle_data, idx_1, idx_2, lower_threshold,
-upper_threshold, fish1_pos, fish2_pos):
-    angle = get_antiparallel_angle(angle_data, idx_1, idx_2)
+
+def get_head_distance_traveled(fish_pos, idx_1, idx_2):
+    initial_x = fish_pos[:, 0][idx_1]
+    initial_y = fish_pos[:, 1][idx_1]
+    final_x = fish_pos[:, 0][idx_2]
+    final_y = fish_pos[:, 1][idx_2]
+    dist = np.sqrt((final_x - initial_x)**2 + (final_y - initial_y)**2)
+    return dist
+
+
+def check_antiparallel_criterion(fish1_angle_data, fish2_angle_data,
+idx_1, idx_2, lower_threshold, upper_threshold, fish1_pos, fish2_pos,
+head_dist_threshold):
+    angle = get_antiparallel_angle(fish1_angle_data, fish2_angle_data, idx_1, idx_2)
     head_distance = get_head_distance(fish1_pos, fish2_pos, idx_1, idx_2)
 
-    if lower_threshold <= angle < upper_threshold and head_distance < 150:
+    if (lower_threshold <= angle < upper_threshold and 
+    head_distance < head_dist_threshold):
         res = True
     else:
         res = False
