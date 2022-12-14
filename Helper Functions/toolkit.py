ce868ec727eb70a5eb68a890544927e9e0579dc5
Parthasarathy Lab, Dept. of Physics, University of Oregon <rplab@users.noreply.github.com>
2022-12-14 11:55:00 -0800

    Update toolkit.py

--- Helper Functions/toolkit.py
+++ Helper Functions/toolkit.py
@@ -34,51 +34,46 @@ def load_data(dir, idx_1, idx_2):
     return fish1_data, fish2_data
 
 
-def get_antiparallel_angle(fish1_angle_data, fish2_angle_data, idx_1, idx_2):
+def get_antiparallel_angle(fish1_angles, fish2_angles):
     """
     Returns the angle for two fish to be in a antiparallel 
     orientation averaged over some window size. 
 
     Args:
-        fish1_angle_data (array): a 1D array of angles at each 
-                                  window frame for fish1.
-        fish2_angle_data (array): a 1D array of angles at each 
-                                  window frame for fish2.
-        idx_1 (int)             : starting index.
-        idx_2 (int)             : ending index.
+        fish1_angles (array): a 1D array of angles for x 
+                              window frames for fish1.
+        fish2_angles (array): a 1D array of angles for x 
+                              window frames for fish2.
 
     Returns:
         theta_avg (float): the angle for two fish to be in a 
                            antiparallel orientation averaged over 
                            some window size.
     """
-    fish1_data = fish1_angle_data[idx_1:idx_2]
-    fish2_data = fish2_angle_data[idx_1:idx_2]
-    theta_avg = np.mean(np.cos(np.subtract(fish1_data, fish2_data)))
+    theta_avg = np.mean(np.cos(np.subtract(fish1_angles, fish2_angles)))
     return theta_avg
 
 
-def get_head_distance(fish1_pos, fish2_pos, idx_1, idx_2):
+def get_head_distance(fish1_positions, fish2_positions):
     """
     Returns the head distance between two fish averaged 
     over some window size.
 
     Args:
-        fish1_pos (array): a 2D array of (x, y) positions for fish1. The
-                           array has form [[x1, y1], [x2, y2], [x3, y3],...].
-        fish2_pos (array): a 2D array of (x, y) positions for fish2. The
-                           array has form [[x1, y1], [x2, y2], [x3, y3],...].
-        idx_1 (int)      : starting index.
-        idx_2 (int)      : ending index.
+        fish1_positions (array): a 2D array of (x, y) positions for fish1 for x
+                                 window frames. The array has form 
+                                 [[x1, y1], [x2, y2], [x3, y3],...].
+        fish2_positions (array): a 2D array of (x, y) positions for fish2 for x
+                                 window frames. The array has form 
+                                 [[x1, y1], [x2, y2], [x3, y3],...].
 
     Returns:
         avg_dist (float) : head distance between two fish averaged 
                            over some window size.
     """
-    head_dist_matrix = np.sqrt((fish2_pos[:, 0] - fish1_pos[:, 0])**2 + 
-    (fish2_pos[:, 1] - fish1_pos[:, 1])**2)
-    dist_temp = head_dist_matrix[idx_1:idx_2]
-    avg_dist = np.average(dist_temp, axis=0)
+    head_dist = np.sqrt((fish2_positions[:, 0] - fish1_positions[:, 0])**2 + 
+    (fish2_positions[:, 1] - fish1_positions[:, 1])**2)
+    avg_dist = np.average(head_dist, axis=0)
     return avg_dist
 
 
@@ -96,7 +91,6 @@ def get_head_distance_traveled(fish_pos, idx_1, idx_2):
     Returns:
         dist (float)    : relative head distance traveled by a single fish 
                           averaged over some window size.
-    
     """
     initial_x = fish_pos[:, 0][idx_1]
     initial_y = fish_pos[:, 1][idx_1]
@@ -106,58 +100,51 @@ def get_head_distance_traveled(fish_pos, idx_1, idx_2):
     return dist
 
 
-def get_fish_vectors(fish1_angle_data, fish2_angle_data, idx_1, idx_2):
+def get_fish_vectors(fish1_angles, fish2_angles):
     """
     Returns a vector in the form (cos(theta), sin(theta)) for fish1
     and fish2 given angle (theta) averaged over some window size.
 
-    Args: 
-        fish1_angle_data (array): a 1D array of angles at each window frame
-                                  for fish1.
-        fish2_angle_data (array): a 1D array of angles at each window frame
-                                  for fish2.
-        idx_1 (int)             : starting index.
-        idx_2 (int)             : ending index.
+    Args:
+        fish1_angles (array): a 1D array of angles for x 
+                              window frames for fish1.
+        fish2_angles (array): a 1D array of angles for x 
+                              window frames for fish2.
 
     Returns:
         A 1D array of vectors of the form [fish1_vector, fish2_vector]. 
     """
-    fish1_data = fish1_angle_data[idx_1:idx_2]
-    fish2_data = fish2_angle_data[idx_1:idx_2]
-    fish1_vector = np.array((np.mean(np.cos(fish1_data)), 
-    np.mean(np.sin(fish1_data))))
-    fish2_vector = np.array((np.mean(np.cos(fish2_data)), 
-    np.mean(np.sin(fish2_data))))
+    fish1_vector = np.array((np.mean(np.cos(fish1_angles)), 
+    np.mean(np.sin(fish1_angles))))
+    fish2_vector = np.array((np.mean(np.cos(fish2_angles)), 
+    np.mean(np.sin(fish2_angles))))
     return np.array((fish1_vector, fish2_vector))
 
 
-def check_antiparallel_criterion(fish1_angle_data, fish2_angle_data,
-idx_1, idx_2, lower_threshold, upper_threshold, fish1_pos, fish2_pos,
-head_dist_threshold):
+def check_antiparallel_criterion(fish1_positions, fish2_positions, fish1_angles, 
+fish2_angles, lower_threshold, upper_threshold, head_dist_threshold):
     """
     Returns True if two fish are antiparallel to each other;
     False otherwise.
 
     Args:
-        fish1_angle_data (array) : a 1D array of angles at each window frame
-                                  for fish1.
-        fish2_angle_data (array) : a 1D array of angles at each window frame
-                                  for fish2.
-        idx_1 (int)              : starting index.
-        idx_2 (int)              : ending index.
+        fish1_positions (array): a 2D array of (x, y) positions for fish1 for x
+                                 window frames. The array has form 
+                                 [[x1, y1], [x2, y2], [x3, y3],...].
+        fish2_positions (array): a 2D array of (x, y) positions for fish2 for x
+                                 window frames. The array has form 
+                                 [[x1, y1], [x2, y2], [x3, y3],...].
+        fish1_angles (array): a 1D array of angles for x window frames for fish1.
+        fish2_angles (array): a 1D array of angles for x window frames for fish2.
         lower_threshold (float)  : lower bound for antiparallel angle.
         upper_threshold (float)  : upper bound for antiparallel angle.
-        fish1_pos (array)        : a 2D array of (x, y) positions for fish1. The
-                                  array has form [[x1, y1], [x2, y2], [x3, y3],...].
-        fish2_pos (array)        : a 2D array of (x, y) positions for fish2. The
-                                  array has form [[x1, y1], [x2, y2], [x3, y3],...].
         head_dist_threshold (int): head distance threshold for the two fish.
 
     Returns:
         True if the fish are antiparallel; False otherwise.
     """
-    angle = get_antiparallel_angle(fish1_angle_data, fish2_angle_data, idx_1, idx_2)
-    head_distance = get_head_distance(fish1_pos, fish2_pos, idx_1, idx_2)
+    angle = get_antiparallel_angle(fish1_angles, fish2_angles)
+    head_distance = get_head_distance(fish1_positions, fish2_positions)
 
     if (lower_threshold <= angle < upper_threshold and 
     head_distance < head_dist_threshold):
