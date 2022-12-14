2307c389cbbe40b4d66d627cc4cdbf00f3420485
Parthasarathy Lab, Dept. of Physics, University of Oregon <rplab@users.noreply.github.com>
2022-12-14 11:47:00 -0800

    Update tail_rubbing.py

--- tail-rubbing/tail_rubbing.py
+++ tail-rubbing/tail_rubbing.py
@@ -1,4 +1,3 @@
-
 # !/usr/bin/env python3  
 # -*- coding: utf-8 -*- 
 #----------------------------------------------------------------------------
@@ -9,54 +8,9 @@
 from toolkit import *
 # ------------------------------------------------------------------------------
 
-def get_contact_wf(body1_x, body2_x, body1_y, body2_y, end, window_size,
-contact_dist):
-    """
-    Returns a dictionary of window frames for different types of
-    contact between two fish.
-
-    Args:
-        body1_x (array): a 1D array of x positions along the 10 body markers of fish1.
-        body2_x (array): a 1D array of x positions along the 10 body markers of fish2.
-        body1_y (array): a 1D array of y positions along the 10 body markers of fish1.
-        body2_y (array): a 1D array of y positions along the 10 body markers of fish2.
-        end (int): end of the array for both fish (typically 15,000 window frames.) 
-        window_size (int): window size for which circling is averaged over.
-        contact_dist (int): the contact distance threshold.
-
-    Returns:
-        contact_wf (dictionary): a dictionary of arrays of different contact types.
-    """
-    idx_1, idx_2 = 0, window_size
-    contact_wf = {"any": np.array([]), "head-body": np.array([])}
-
-    for i in range(end // window_size):
-        # Head-body contact
-        if (np.min(np.sqrt((body1_x[idx_1][0] - body2_x[idx_1])**2 + 
-        (body1_y[idx_1][0] - body2_y[idx_1])**2)) < contact_dist or 
-        np.min(np.sqrt((body1_x[idx_1] - body2_x[idx_1][0])**2 + 
-        (body1_y[idx_1] - body2_y[idx_1][0])**2) < contact_dist)):
-             contact_wf["head-body"] = np.append(contact_wf["head-body"], idx_2)
-             contact_wf["any"] = np.append(contact_wf["any"], idx_2)
-
-        # Any contact 
-        for j in range(1, 10):  # there are ten markers on the fish bodies
-            if (np.min(np.sqrt((body1_x[idx_1][j] - body2_x[idx_1])**2 + 
-            (body1_y[idx_1][j] - body2_y[idx_1])**2)) < contact_dist or 
-            np.min(np.sqrt((body1_x[idx_1] - body2_x[idx_1][j])**2 + 
-            (body1_y[idx_1] - body2_y[idx_1][j])**2) < contact_dist)) and (idx_2 not in
-            contact_wf["any"]):
-                contact_wf["any"] = np.append(contact_wf["any"], idx_2)
-
-        # Update the index variables to track contact for the 
-        # next x window frames of size window_size
-        idx_1, idx_2 = idx_1+window_size, idx_2+window_size
-    return contact_wf
-
-
 def get_tail_rubbing_wf(body1_x, body2_x, body1_y, body2_y, fish1_pos, fish2_pos,
 fish1_angle_data, fish2_angle_data, end, window_size, tail_dist, tail_anti_low, 
-tail_anti_high, head_dist): 
+tail_anti_high, head_dist_thresh): 
     """
     Returns an array of tail-rubbing window frames.
 
@@ -88,19 +42,25 @@ tail_anti_high, head_dist):
     tail_rubbing_wf = np.array([])
 
     for i in range (end // window_size):
+        # Get 4 posterior body markers for x window frames 
         tail1_x = np.average(body1_x[:, -4:][idx_1:idx_2], axis=0)
         tail2_x = np.average(body2_x[:, -4:][idx_1:idx_2], axis=0)
         tail1_y = np.average(body1_y[:, -4:][idx_1:idx_2], axis=0)
         tail2_y = np.average(body2_y[:, -4:][idx_1:idx_2], axis=0)
+
+        # Get position and angle data for x window frames 
+        fish1_positions = fish1_pos[idx_1:idx_2]
+        fish2_positions = fish2_pos[idx_1:idx_2]
+        fish1_angles = fish1_angle_data[idx_1:idx_2]
+        fish2_angles = fish2_angle_data[idx_1:idx_2]
       
         for j in range(4):   # get at least four body points in contact
             min_dist = get_min_tail_distances(tail1_x, tail2_x, tail1_y,
             tail2_y, j)
             if (min_dist[0] < tail_dist and min_dist[1] < tail_dist or 
             min_dist[2] < tail_dist and min_dist[3] < tail_dist and
-            check_antiparallel_criterion(fish1_angle_data, fish2_angle_data, 
-            idx_1, idx_2, tail_anti_low, tail_anti_high, fish1_pos, fish2_pos,
-            head_dist)):
+            check_antiparallel_criterion(fish1_positions, fish2_positions, fish1_angles, 
+            fish2_angles, tail_anti_low, tail_anti_high, head_dist_thresh)):
                 if idx_2 not in tail_rubbing_wf:
                     tail_rubbing_wf = np.append(tail_rubbing_wf, idx_2)
 
