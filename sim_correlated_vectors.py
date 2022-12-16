37b9b1cfe5b397654ac762f2a3277af306a1e1ec
Parthasarathy Lab, Dept. of Physics, University of Oregon <rplab@users.noreply.github.com>
2022-12-16 12:01:05 -0800

    Update sim_correlated_vectors.py
    Finish filtering vectors, simulating cross-correlation.

--- sim_correlated_vectors.py
+++ sim_correlated_vectors.py
@@ -3,7 +3,7 @@
 """
 Author:   Raghuveer Parthasarathy
 Created on Mon Dec 12 11:27:36 2022
-Last modified on Dec. 15, 2022
+Last modified on Dec. 16, 2022
 
 Description
 -----------
@@ -112,24 +112,65 @@ print(f'   alpha1_est : {alpha1_est:.3f} +/- {alpha1_est_sigma:.3f}')
 print(f'   alpha2_est : {alpha2_est:.3f} +/- {alpha2_est_sigma:.3f}')
 print(f'   two sigma (95% CI) value for alpha == 0: {2*alpha_zero_sigma:.3f}')
 
+#%% for simple 1D smoothing
+
+# 3 px, simple average
+# smooth_kernel = np.array([0.212, 0.576, 0.212]) Gaussian, not as good
+smooth_kernel = np.array([0.25, 0.5, 0.25])
+
+
 #%% Assess *apparent* cross-correlation
 
 # Note that x1, x2 are uncorrelated!
 N = len(x1)
 ccf = np.correlate(x1, x2, mode='full')/np.std(x1)/np.std(x2)/N
 
+# Filter x1, x2 by subtracting the AR(1) model
+
+def calc_AR1_residual(x, alpha):
+    # subtracting the AR(1) model with parameter alpha from series x
+    x = x - np.mean(x)
+    for j in np.arange(1, len(x)):
+        x[j] = x[j] - alpha*(x[j-1])
+    return x
+
+x1p = calc_AR1_residual(x1, alpha1_est)
+x2p = calc_AR1_residual(x2, alpha2_est)
+
+#Plot Time Series
+plt.figure()
+plt.plot(t, x1, color=(0.2, 0.4, 0.9), label='x1')
+plt.plot(t, x2, color=(0.3, 0.8, 0.3), label='x2')
+plt.plot(t, x1p, color=(0.2, 0.2, 0.4), label='x1p')
+plt.plot(t, x2p, color=(0.3, 0.5, 0.1), label='x2p')
+plt.xlabel('time (index)')
+plt.legend()
+
+# Cross-correlation of the filtered signals
+ccf_filtered = np.correlate(x1p, x2p, mode='full')/np.std(x1p)/np.std(x2p)/N
+
 # one sigma value for no cross-correlation
 ccf_zero_sigma = 1/np.sqrt(len(x1))
 
-k_array = np.arange(-(N-1),N)
+lag_max = int(np.round(N/5)) # Just plot ccf to +/- this point
+k_array = np.arange(-lag_max, lag_max + 1)
+k_array_ind = k_array + N - 1
 plt.figure()
-plt.plot(k_array, ccf)
-plt.plot(k_array, ccf_zero_sigma*np.ones_like(k_array), color=[0.4, 0.4, 0.4])
-plt.plot(k_array, -ccf_zero_sigma*np.ones_like(k_array), color=[0.4, 0.4, 0.4])
+plt.plot(k_array, ccf[k_array_ind], label='original x1, x2')
+plt.plot(k_array, ccf_filtered[k_array_ind], label='filtered x1p, x2p')
+plt.plot(k_array, np.convolve(ccf_filtered[k_array_ind], 
+                              smooth_kernel, mode='same'), 
+         label='filtered x1p, x2p; Smoothed', color=[0.7, 0.2, 0.1])
+plt.plot(k_array, 2*ccf_zero_sigma*np.ones_like(k_array_ind), 
+         color=[0.4, 0.4, 0.4], label='95% conf. of 0')
+plt.plot(k_array, -2*ccf_zero_sigma*np.ones_like(k_array_ind), 
+         color=[0.4, 0.4, 0.4])
 plt.xlabel('lag, k')
 plt.ylabel('Cross-correlation')
+plt.title(f'No true Cross-Correlation! alpha = {alpha:.3f}')
+plt.legend()
 
-#%% Cross-correlated vectors
+#%% Cross-correlated vectors, simulate
 
 rho_C = 0.3 # cross-correlation magnitude
 
@@ -137,15 +178,40 @@ rho_C = 0.3 # cross-correlation magnitude
 y1 = x1.copy()
 y2 = x1*rho_C + x2*np.sqrt(1 - rho_C**2)
 
-#%% Plots
+# Calculate autocorrelation
+y1_acf = calc_acf(y1)
+y2_acf = calc_acf(y2)
 
-t = np.arange(N)
+# Estimate autocorrelation parameter
+y1_alpha_est = y1_acf[1]/y1_acf[0]
+y2_alpha_est = y2_acf[1]/y2_acf[0]
+
+# Filter
+y1p = calc_AR1_residual(y1, y1_alpha_est)
+y2p = calc_AR1_residual(y2, y2_alpha_est)
+
+# Cross-correlation of the unfiltered and filtered signals
+y_ccf = np.correlate(y1, y2, mode='full')/np.std(y1)/np.std(y2)/N
+y_ccf_filtered = np.correlate(y1p, y2p, mode='full')/np.std(y1p)/np.std(y2p)/N
+
+# one sigma value for no cross-correlation
+ccf_zero_sigma = 1/np.sqrt(len(y1))
+
+lag_max = int(np.round(N/5)) # Just plot ccf to +/- this point
+k_array = np.arange(-lag_max, lag_max + 1)
+k_array_ind = k_array + N - 1
 plt.figure()
-plt.plot(t, y1, color=(0.6, 0.8, 0.7), label='x1')
-#plt.plot(t, x2, color=(0.7, 0.6, 0.8), label='x2')
-plt.plot(t, y2, color=(0.2, 0.8, 0.9), label='y1')
-#plt.plot(t, y2, color=(0.3, 0.8, 0.7), label='y2')
-#plt.plot(t, v1, color=(0.8, 0.2, 0.1), label='v1==y1', linewidth=2.0)
-#plt.plot(t, v2, color=(0.9, 0.6, 0.2), label='v2', linewidth=2.0)
-plt.xlabel('time (index)')
+plt.plot(k_array, y_ccf[k_array_ind], label='NOT filtered y1, y2')
+plt.plot(k_array, y_ccf_filtered[k_array_ind], label='filtered y1p, y2p')
+plt.plot(k_array, np.convolve(y_ccf_filtered[k_array_ind], 
+                              smooth_kernel, mode='same'), 
+         label='filtered y1p, xy2p; Smoothed', color=[0.7, 0.2, 0.1])
+plt.plot(k_array, 2*ccf_zero_sigma*np.ones_like(k_array_ind), 
+         color=[0.4, 0.4, 0.4], label='95% conf. of 0')
+plt.plot(k_array, -2*ccf_zero_sigma*np.ones_like(k_array_ind), 
+         color=[0.4, 0.4, 0.4])
+plt.xlabel('lag, k')
+plt.ylabel('Cross-correlation')
+plt.title(f'True cross-correlation; alpha = {alpha:0.3f}, rho_C = {rho_C:.3f}')
 plt.legend()
+
